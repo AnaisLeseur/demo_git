e716c6329c8a51b68aa415b98915b658048472a5
anais leseur <anais.leseur@gmail.com>
2020-07-02 11:29:15 +0200

    ajout des props de Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -1,4 +1,15 @@
 
+/**
+ * classe de l'utilisateur 
+ * @author vincent
+ *
+ */
 public class Utilisateur {
+	
+	// ----- props -----
+	private int id;
+	private String nom;
+	private String prenom;
+	
 
-}
+}// end class
