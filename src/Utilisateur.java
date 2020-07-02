13ac39acbff74d0c6faf0436040e8fc0b57dbbc1
anais leseur <anais.leseur@gmail.com>
2020-07-02 11:34:55 +0200

    ajout des ctors de Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -10,6 +10,19 @@ public class Utilisateur {
 	private int id;
 	private String nom;
 	private String prenom;
+
+	
+	// ----- ctors -----
+	public Utilisateur() {
+	}
+	
+	public Utilisateur(int id, String nom, String prenom) {
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	
+	
 	
 
 }// end class
